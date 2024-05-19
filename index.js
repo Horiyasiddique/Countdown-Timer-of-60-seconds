b71bb7596cba0ba728e0ef1bd33d6aeaa2189c82
Hooriya Siddique <160617521+Horiyasiddique@users.noreply.github.com>
2024-05-19 17:55:06 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { differenceInSeconds } from "date-fns";
+const res = await inquirer.prompt([
+    {
+        name: "userInput",
+        type: "number",
+        message: chalk.blue(" \n\tplease enter the seconds\n\t"),
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please enter valid number";
+            }
+            else if (input > 60) {
+                return "seconds must be in 60";
+            }
+            else {
+                return true;
+            }
+        }
+    }
+]);
+let input = res.userInput;
+function startTime(val) {
+    const inTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(inTime);
+    setInterval((() => {
+        const currTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currTime);
+        if (timeDiff <= 0) {
+            console.log(chalk.red("\n\tTimer has expired!!!!"));
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")} : ${sec.toString().padStart(2, "0")}`);
+    }), 1000);
+}
+startTime(input);
